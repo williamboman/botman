2123bbd11bd57e6b90ec17917c83d7792ef3642e
William Boman <william@redwill.se>
2024-01-28 14:43:03 +0100

    fix: fix applying whitespace

--- src/mason_registry/fixup.rs
+++ src/mason_registry/fixup.rs
@@ -1,9 +1,8 @@
 use anyhow::Result;
-use std::{collections::HashSet, fmt::Display, path::PathBuf};
-
 use async_recursion::async_recursion;
 use lazy_static::__Deref;
 use rocket::http::Status;
+use std::{collections::HashSet, fmt::Display, path::PathBuf};
 use tokio::{
     fs::{self, DirEntry, File},
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
@@ -75,10 +74,34 @@ async fn yml_to_yaml(workspace: &Workspace, changed_files: &HashSet<PathBuf>) ->
     Ok(())
 }
 
-async fn fix_styling(
-    workspace: &Workspace,
-    changed_files: &HashSet<PathBuf>,
-) -> Result<()> {
+fn apply_styling_fixes<'a>(lines: &'a Vec<String>) -> Vec<&'a str> {
+    let mut new_file_lines = vec![];
+    let mut it = lines.windows(2).peekable();
+    while let Some(slice) = it.next() {
+        if let (Some(line1), Some(line2)) = (slice.get(0), slice.get(1)) {
+            match (line1.as_str(), line2.as_str()) {
+                (line1, _line2 @ ("source:" | "bin:" | "share:" | "opt:")) if line1 != "" => {
+                    new_file_lines.push(line1);
+                    new_file_lines.push("");
+                }
+                (line1, _) => {
+                    new_file_lines.push(line1);
+                }
+            }
+            if it.peek().is_none() {
+                new_file_lines.push(line2);
+            }
+        } else {
+            new_file_lines.extend(slice.iter().map(|s| s.as_str()));
+        }
+    }
+    if new_file_lines.get(0) != Some(&"---") {
+        new_file_lines.insert(0, "---");
+    }
+    return new_file_lines;
+}
+
+async fn fix_styling(workspace: &Workspace, changed_files: &HashSet<PathBuf>) -> Result<()> {
     let mut packages_dir = workspace.workdir.path().to_path_buf();
     packages_dir.push("packages");
     let mut entries = vec![];
@@ -98,27 +121,8 @@ async fn fix_styling(
             lines.push(line);
         }
 
-        let mut new_file_lines = vec![];
-        if lines.get(0) != Some(&"---".to_string()) {
-            new_file_lines.push("---");
-        }
-        for slice in lines.chunks(2) {
-            if let (Some(line1), Some(line2)) = (slice.get(0), slice.get(1)) {
-                match (line1.as_str(), line2.as_str()) {
-                    (line1, line2 @ ("source:" | "bin:" | "share:" | "opt:")) if line1 != "" => {
-                        new_file_lines.push(line1);
-                        new_file_lines.push("");
-                        new_file_lines.push(line2);
-                    }
-                    (line1, line2) => {
-                        new_file_lines.push(line1);
-                        new_file_lines.push(line2);
-                    }
-                }
-            } else {
-                new_file_lines.extend(slice.iter().map(|s| s.as_str()));
-            }
-        }
+        let new_file_lines = apply_styling_fixes(&lines);
+
         let mut new_file = File::create(&entry_path).await?;
         for line in new_file_lines {
             new_file.write_all(line.as_bytes()).await?;
@@ -173,4 +177,84 @@ pub(super) async fn run(
         "Successfully ran mason-registry fixup in {:?}",
         workspace
     )))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::apply_styling_fixes;
+    use indoc::indoc;
+
+    fn from_str(str: &str) -> Vec<String> {
+        str.split("\n").map(ToOwned::to_owned).collect()
+    }
+
+    fn from_vec(vec: &Vec<&str>) -> String {
+        vec.join("\n")
+    }
+
+    #[test]
+    fn it_should_apply_yaml_document_header() {
+        assert_eq!(
+            indoc! {"
+            ---
+            name: lua-language-server
+        "},
+            from_vec(&apply_styling_fixes(&from_str(indoc! {"
+            name: lua-language-server
+        "})))
+        );
+    }
+
+    #[test]
+    fn it_should_apply_whitespaces() {
+        assert_eq!(
+            indoc! {r#"
+                ---
+                name: mdslw
+                description: The MarkDown Sentence Line Wrapper, an auto-formatter that prepares your markdown for easy diff'ing.
+                homepage: https://github.com/razziel89/mdslw
+                licenses:
+                  - GPL-3.0-only
+                languages:
+                  - Markdown
+                categories:
+                  - Formatter
+
+                source:
+                  id: pkg:github/razziel89/mdformat@0.6.1
+                  asset:
+                    - target: darwin_x64
+                      file: mdslw_x86_64-apple-darwin
+                    - target: linux_x64
+                      file: mdslw_x86_64-unknown-linux-musl
+                    - target: win_x64
+                      file: mdslw_x86_64-pc-windows-gnu.exe
+
+                bin:
+                  mdslw: "{{source.asset.file}}"
+        "#},
+            from_vec(&apply_styling_fixes(&from_str(indoc! {r#"
+                name: mdslw
+                description: The MarkDown Sentence Line Wrapper, an auto-formatter that prepares your markdown for easy diff'ing.
+                homepage: https://github.com/razziel89/mdslw
+                licenses:
+                  - GPL-3.0-only
+                languages:
+                  - Markdown
+                categories:
+                  - Formatter
+                source:
+                  id: pkg:github/razziel89/mdformat@0.6.1
+                  asset:
+                    - target: darwin_x64
+                      file: mdslw_x86_64-apple-darwin
+                    - target: linux_x64
+                      file: mdslw_x86_64-unknown-linux-musl
+                    - target: win_x64
+                      file: mdslw_x86_64-pc-windows-gnu.exe
+                bin:
+                  mdslw: "{{source.asset.file}}"
+        "#})))
+        );
+    }
 }
